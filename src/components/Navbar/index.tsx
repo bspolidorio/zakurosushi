7ba36b6c7876a3fc5094d28423e80024dca19403
Bruno Spolidorio <bruno.bachega-spolidorio-ext@ledger.fr>
2022-11-22 21:54:00 +0000

    first version

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="flex flex-col justify-center h-16">
+      <img className="" src="/assets/logo.png" alt="logo" />
+    </div>
+  );
+};
+
+export default Navbar;
