8ea327efa6d772a889818a59160dc03e5054696c
Bruno Spolidorio <bruno.bachega-spolidorio-ext@ledger.fr>
2023-01-05 14:17:48 +0000

    fix: menu close button

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useOnClickOutside } from "../../Shared/hooks";
 import Burger from "../Burger";
 import Menu from "../Menu";
 import * as S from "./styles";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const node = useRef(null);
+  useOnClickOutside(node, () => setOpen(false));
 
   return (
     <S.Header>
       <S.LogoContainer>ZAKURO SUSHI</S.LogoContainer>
-      <Burger open={open} setOpen={setOpen} />
-      <Menu open={open} setOpen={setOpen} />
+      <div ref={node}>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu open={open} />
+      </div>
     </S.Header>
   );
 };
--- src/Components/Menu/index.tsx
+++ src/Components/Menu/index.tsx
@@ -1,17 +1,12 @@
-import { useRef } from "react";
-import useOnClickOutside from "../../Shared/hooks";
 import * as S from "./styles";
 
 interface Props {
   open: boolean;
-  setOpen: (value: boolean) => void;
 }
 
-const Navigation = ({ open, setOpen }: Props) => {
-  const node = useRef(null);
-  useOnClickOutside(node, () => setOpen(false));
+const Navigation = ({ open }: Props) => {
   return (
-    <S.Menu ref={node} open={open}>
+    <S.Menu open={open}>
       <a href="/">Home</a>
       <a href="/">Cardápio</a>
       <a href="/">Sobre</a>
